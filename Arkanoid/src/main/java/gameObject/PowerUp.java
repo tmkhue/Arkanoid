dbbee9f01691f3fc60a97456ecedaec25f21948a
bichphuong-jun <phuonghoanhsona1@gmail.com>
2025-10-04 13:48:04 +0700

    Vật phẩm powerup

---
+++ Arkanoid/src/main/java/gameObject/PowerUp.java
@@ -0,0 +1,29 @@
+package gameObject;
+
+public class PowerUp extends GameObject {
+    protected double type;
+    protected double duration;
+
+    public double getType() {
+        return type;
+    }
+
+    public void setType(double type) {
+        this.type = type;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public PowerUp(double x, double y, double width, double height, double type, double duration) {
+        super(x, y, width, height);
+        this.type = type;
+        this.duration = duration;
+    }
+
+}
