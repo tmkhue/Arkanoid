c70e61afe7bdf591b625de28967d4ee141c12971
bichphuong-jun <phuonghoanhsona1@gmail.com>
2025-11-07 15:28:07 +0700

    test

--- game/src/main/java/org/example/game/PowerUpFactory.java
+++ game/src/main/java/org/example/game/PowerUpFactory.java
@@ -6,14 +6,16 @@ import java.util.List;
 
 public class PowerUpFactory {
     public static PowerUp createPowerUp(double x, double y, Pane gamePane, List<Ball> balls, Paddle paddle, PaddleResizer paddleResizer) {
-        if (Math.random() < 0.2) {
+        if (Math.random() < 1.0 / 6.0) {
             return new FastBallPowerUp(x, y, 15);
-        } else if (Math.random() < 0.4) {
+        } else if (Math.random() < 2.0 / 6.0) {
             return new TripleBallPowerUp(x, y, 15, gamePane, balls);
-        } else if (Math.random() < 0.6) {
+        } else if (Math.random() < 3.0 / 6.0) {
             return new StrongBallPowerUp(x, y, 15);
-        } else {
+        } else if (Math.random() < 4.0 / 6.0) {
             return new WidenPaddlePowerUp(x, y, 10, paddleResizer);
+        } else {
+            return new ExtraLifePowerUp(x, y);
         }
     }
 }
